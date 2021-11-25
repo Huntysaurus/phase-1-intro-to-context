5de7a05ab60b281d9c9136e5ca7d51b4a65888a6
Jacob <yawjacob@yahoo.com>
2021-11-25 17:01:00 -0600

    commit tree 898daf2a49d5d1db572bacc303ca35d86a968f30

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function createEmployeeRecord(array) {
+    const obj = {}
+    for(let i = 0; i < array.length; i++) {
+        obj.append(array[i])
+        console.log(obj)
+    }
+}
+
+
+createEmployeeRecord([1,2,3,4])
